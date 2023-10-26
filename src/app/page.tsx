68a9e5af5633ab7ad5e70aeca9b204c4dd7e6f1f
rizrmdhn <rizrmdhn.work@gmail.com>
2023-10-26 10:01:20 +0800

    change main height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Project from "@/components/Project";
 
 export default function Home() {
   return (
-    <main className="flex flex-col bg-primary-foreground pb-5 pe-10 ps-10 pt-5 duration-300 sm:pe-24 sm:ps-24">
+    <main className="flex h-screen flex-col overflow-auto bg-primary-foreground pb-5 pe-10 ps-10 pt-5 duration-300 sm:pe-24 sm:ps-24">
       <Header />
       <section className="flex flex-col items-center justify-center">
         <About />
